b374129b1f12e9b3e4fd77c505e74e369be9d22f
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-03-07 01:41:15 +0000

    Set up enneagon_prism_calc.py and added header comments.

---
+++ enneagon_prism_calc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Created By: Joseph Wondimagnehu
+# Date: Mar. 6, 2025
+"""
+This program allows the user to calculate the volume, 
+surface area, lateral surface area, and base area of
+an enneagonal prism by asking them for the base edge and 
+height, or just the base edge.
+"""
+
